89ec92075c098adf8f1e8f1adfc1c406e2d69aec
Jeamay Palo <jeamay.palo@gmail.com>
2018-04-20 03:11:27 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO authors (id, name) VALUES (1, "J.K. Rowling"), (2, "Second Author");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "The Sorcerer's Stone", 1997, 1), (2, "A Chamber of Secrets", 1998, 1), (3, "Prisoner of Azkaban", 1999, 1), (4, "Dune", 1965, 2), (5, "Dune Messiah", 1969, 2), (6, "Songs of Muad'dib", 1992, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Harry Potter", "Kill Voldemort", "Halfblood", 1, 1), (2, "Ron Weasley", "I hate Draco", "Pureblood", 1, 1), (3, "Hermione Granger", "Books save!", "Mudblood", 1, 1), (4, "Dumbledore", "I love Grindelwald", "wizard", 1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Harry Potter", "Kill Voldemort", "halfblood wizard", 1, 1), (2, "Ron Weasley", "I hate Draco", "pureblood wizard", 1, 1), (3, "Hermione Granger", "Books save!", "mudblood wizard", 1, 1), (4, "Dumbledore", "I love Grindelwald", "wizard", 1, 1);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Duke Leto Atreides", "long live House Atreides", "human", 2, 2), (6, "Paul Atreides", "I am Muad'dib!", "psychic human", 2, 2), (7, "Jessica Atreides", "human", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 
